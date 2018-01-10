820488f4789845eecb9ffa01845ee079bcaed209
Marcy Bursac <msbursac@gmail.com>
2018-01-09 22:36:40 -0600

    Index and About up and running!

--- GymRat/Controllers/HomeController.cs
+++ GymRat/Controllers/HomeController.cs
@@ -10,28 +10,16 @@ namespace GymRat.Controllers
 {
     public class HomeController : Controller
     {
+        //home screen
         public IActionResult Index()
         {
             return View();
         }
 
+        //about page
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
