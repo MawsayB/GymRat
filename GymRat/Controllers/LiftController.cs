98c2e257eb2ada80d5835d6c4e2723a853beb02a
Marcy Bursac <msbursac@gmail.com>
2018-01-10 23:51:13 -0600

    Lift menu page built

---
+++ GymRat/Controllers/LiftController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GymRat.Models;
+
+namespace GymRat.Controllers
+{
+    public class LiftController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Workout()
+        {
+            return View();
+        }
+
+    }
+}
