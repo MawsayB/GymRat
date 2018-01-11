e7c88a8bfa11a761833e0edb92fe7936bd1c15e6
Marcy Bursac <msbursac@gmail.com>
2018-01-10 23:00:47 -0600

    Built out statistics page to display all exercise start and high weight to user

--- GymRat/Controllers/AnalyzeController.cs
+++ GymRat/Controllers/AnalyzeController.cs
@@ -12,5 +12,10 @@ namespace GymRat.Controllers
         {
             return View();
         }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
     }
 }
