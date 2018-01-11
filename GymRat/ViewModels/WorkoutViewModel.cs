c3e5b13c17acdb71716521516457d598ebd44388
Marcy Bursac <msbursac@gmail.com>
2018-01-11 00:26:03 -0600

    More on Lift Workout Page layout

---
+++ GymRat/ViewModels/WorkoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.ViewModels
+{
+    public class WorkoutViewModel
+    {
+        public int Reps { get; set; }
+        public int Weight { get; set; }
+    }
+}
