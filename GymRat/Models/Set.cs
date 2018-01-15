766d5cd8567dbd2df9a9cb6cc32d686b1966d8d8
Marcy Bursac <msbursac@gmail.com>
2018-01-15 07:48:15 -0600

    Created models for Activity, Set and Workout

---
+++ GymRat/Models/Set.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.Models
+{
+    public class Set
+    {
+        public int ID { get; set; }
+        public int WorkoutID { get; set; }
+        public int UserID { get; set; }
+
+        public int Rep { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ GymRat/Models/Workout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.Models
+{
+    public class Workout
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public int ActivityID { get; set; }
+    }
+}
