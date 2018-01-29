58ab1bc11433fb8d070f9a99f37e65944d458ace
Marcy Bursac <msbursac@gmail.com>
2018-01-29 11:00:55 -0600

    added enum list of Regions to AddMeasureViewModel

---
+++ GymRat/Models/MeasureRegionOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.Models
+{
+    public enum MeasureRegionOptions
+    {
+        // Enum representing the Body Region options of a Measurement
+        // that can be viewed and searched.
+
+        Butt,
+        Thigh,
+        Waist,
+        All
+    }
+}
--- GymRat/ViewModels/AddMeasureViewModel.cs
+++ GymRat/ViewModels/AddMeasureViewModel.cs
@@ -19,11 +19,24 @@ namespace GymRat.ViewModels
         [Required]
         [Display(Name = "Region")]
         public string Region { get; set; }
+        public List<MeasureRegionOptions> Regions { get; set; }
 
         //size
         [Required]
         [Display(Name = "Current Size")]
         public double Size { get; set; }
 
+        public AddMeasureViewModel()
+        {
+            Regions = new List<MeasureRegionOptions>();
+
+            foreach (MeasureRegionOptions enumVal in Enum.GetValues(typeof(MeasureRegionOptions)))
+            {
+                Regions.Add(enumVal);
+            }
+
+        }
+
     }
+
 }
