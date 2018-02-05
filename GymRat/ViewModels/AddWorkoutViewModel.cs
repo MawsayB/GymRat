e7fe58d94189d40142df6793769e902e7fc9883a
Marcy Bursac <msbursac@gmail.com>
2018-02-05 14:55:28 -0600

    Added date and muscle group to form on Create2

--- GymRat/ViewModels/AddWorkoutViewModel.cs
+++ GymRat/ViewModels/AddWorkoutViewModel.cs
@@ -10,73 +10,17 @@ namespace GymRat.ViewModels
     public class AddWorkoutViewModel
     {
         [Required]
-        [Display(Name = "Workout Name")]
-        public DateTime Name { get; set; }
+        [Display(Name = "Use On")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
 
-        //region - optional input field
-        [Display(Name = "Muscle Group")]
-        public int MuscleGroupID { get; set; }
+        [Required]
+        [Display(Name = "Muscle Group(s)")]
+        public string Muscle { get; set; }
+        public List<MeasureRegionOptions> Muscles { get; set; }
 
         //TODO: get the list of MuscleGroups
         //public List<SelectListItem> MucleGroups { get; set; }
 
-        //activity
-        [Required]
-        [Display(Name = "Activity 1")]
-        public int ActivityID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Activity 2")]
-        //public int ActivityID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Activity 3")]
-        //public int ActivityID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Activity 4")]
-        //public int ActivityID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Activity 5")]
-        //public int ActivityID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Activity 6")]
-        //public int ActivityID { get; set; }
-
-        //# of sets???
-        //TODO: make default 3
-        [Required]
-        [Display(Name = "Number of Sets")]
-        public int Sets { get; set; }
-
-        // # of reps in set
-        [Required]
-        [Display(Name = "Number of Reps")]
-        public int Reps { get; set; }
-
-        public AddWorkoutViewModel()
-
-        {
-
-        }
-
-        //public AddWorkoutViewModel(IEnumerable<MuscleGroup> muscleGroup)
-        //{
-
-            //BodyRegions = new List<SelectListItem>();
-
-            //foreach (var bodyregion in bodyRegions)
-            //{
-                //BodyRegions.Add(new SelectListItem
-                //{
-                    //Value = bodyregion.ID.ToString(),
-                    //Text = bodyregion.Name
-                //});
-
-            //}
-
-        //}
     }
 }
