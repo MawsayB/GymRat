14d72818575dcec13dbd412ae76b694a26136ea8
Marcy Bursac <msbursac@gmail.com>
2018-02-12 13:23:13 -0600

    Populated ExerciseLabels table!

---
+++ dbo.ExerciseLabels(3).data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[ExerciseLabel] ([ID], [Label]) VALUES ([1], ["arms"])
---
+++ dbo.ExerciseLabels.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[ExerciseLabels] ON
+INSERT INTO [dbo].[ExerciseLabels] ([ID], [Label]) VALUES (NULL, NULL)
+SET IDENTITY_INSERT [dbo].[ExerciseLabels] OFF
+
+INSERT INTO [dbo].[ExerciseLabels] ([ID], [Label]) VALUES (1, "arms")
