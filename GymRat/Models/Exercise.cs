91732b0c2e2b1d144bd782ee4e6cc5db1a96f1e4
Marcy Bursac <msbursac@gmail.com>
2018-02-12 13:26:51 -0600

    added comments in Exercise class to explain properties

--- GymRat/Models/Exercise.cs
+++ GymRat/Models/Exercise.cs
@@ -7,8 +7,12 @@ namespace GymRat.Models
 {
     public class Exercise
     {
+        //ExerciseID
         public int ID { get; set; }
         public string Name { get; set; }
+        //LabelID tells which muscle group
         public int LabelID { get; set; }
+        //Type options: weightlifting OR cardio
+        public string Type { get; set; }
     }
 }
