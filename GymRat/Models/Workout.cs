d04d8f9ac5c46fca0a4a8d47ede8b51dfc6bfe39
Marcy Bursac <msbursac@gmail.com>
2018-02-12 13:28:58 -0600

    added Workout class and comments to begin building workouts

---
+++ GymRat/Models/Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.Models
+{
+    public class Workout
+    {
+
+        // a Workout consists of 9 exercises
+        // 6 are weightlifting exercises
+        // 3 are cardio exercises
+    }
+}
