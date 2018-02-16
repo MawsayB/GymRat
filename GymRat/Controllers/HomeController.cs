fe022b902358504a71b9cce09e5351ef4b9e946b
Marcy Bursac <msbursac@gmail.com>
2018-02-16 08:05:52 -0600

    added Authorize to Controllers (Lift and Analyze) and to Menu on Home Controller

--- GymRat/Controllers/HomeController.cs
+++ GymRat/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GymRat.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GymRat.Controllers
 {
@@ -22,6 +23,7 @@ namespace GymRat.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Menu()
         {
             return View();
